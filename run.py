c3f1814dbf409e6718dd607c1738c62b4632bdbd
Ayman <ayman.asbai101@alu.ulpgc.es>
2025-12-12 17:35:19 +0000

    add: develop part 1 execution flow.

--- run.py
+++ run.py
@@ -1,9 +1,7 @@
+import search
 # Search methods
 
-import search
-
-ab = search.GPSProblem('A', 'B'
-                       , search.romania)
+ab = search.GPSProblem('A', 'B', search.romania)
 
 print(search.breadth_first_graph_search(ab).path())
 print(search.depth_first_graph_search(ab).path())
@@ -11,3 +9,5 @@ print(search.depth_first_graph_search(ab).path())
 # Result:
 # [<Node B>, <Node P>, <Node R>, <Node S>, <Node A>] : 101 + 97 + 80 + 140 = 418
 # [<Node B>, <Node F>, <Node S>, <Node A>] : 211 + 99 + 140 = 450
+
+print(search.branch_and_bound_graph_search(ab).path())
